70210db6f9add28304ac7294aa7cc390da21faaa
Rupesh3000 <101381357+Rupesh3000@users.noreply.github.com>
2026-01-20 23:45:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+const speed = document.getElementById("speed");
+const text = document.getElementById("text");
+
+const renderText = "We love Programming!";
+let intervalId = null;
+let i = 0;
+
+
+intervalId = setInterval(() => {
+  text.innerHTML += renderText[i];
+  i++;
+
+  if (i === renderText.length) {
+    clearInterval(intervalId);
+    intervalId = null;
+  }
+}, 500 / Number(speed.value));
